2c3b442cbd817189980bd14ad8d0bb8e2b4ffe95
kilian <kilianrave@gmail.com>
2021-11-08 16:49:16 +0100

    Some changes

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-asdasdhfdahfd
+
 
 print('hello')
