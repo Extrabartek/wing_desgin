7b6143292474706b154396c6f390db9902b255f1
Bartosz Jemioł <ekstrabartek@gmail.com>
2021-11-22 17:19:24 +0100

    Changes by Mailn

--- WP_41.py
+++ WP_41.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import scipy as sp
 from scipy import interpolate
-from scipy import integrate
-import numpy as np
 
-# ----------------Constants------------------
+# ---------------- Values & Constants ------------------
+# Values known from previous work packages, and constants needed
 cr = 6.46
 ct = 1.83464
 b = 31.1
@@ -12,7 +12,7 @@ rho = 1.225
 V = 256
 q = 0.5 * rho * V ** 2
 
-# Reading file and making an array with columns y, cl, cd, cm
+# Reading the data files and making an array with columns y (wing span [m]), cl, cd, cm
 data1 = "MainWing_a=0.00_v=10.00ms.txt"
 data2 = "MainWing_a=10.00_v=10.00ms.txt"
 data3 = "MainWing_a=-10.00_v=10.00ms.txt"
@@ -20,7 +20,7 @@ list1 = np.genfromtxt(data1, dtype='float', skip_header=40, skip_footer=1029, us
 list2 = np.genfromtxt(data2, dtype='float', skip_header=40, skip_footer=1029, usecols=(0, 3, 5, 7))
 list3 = np.genfromtxt(data3, dtype='float', skip_header=40, skip_footer=1029, usecols=(0, 3, 5, 7))
 
-# Making individual lists for values of y, cl, cd, cm
+# Making lists for values of y (wing span [m]), cl, cd, cm at each angle of attack (AoA)
 y = []
 cl1 = []
 cd1 = []
@@ -31,7 +31,6 @@ cm2 = []
 cl3 = []
 cd3 = []
 cm3 = []
-
 for i in range(19):
     y.append(list1[i][0])
     cl1.append(list1[i][1])
@@ -40,9 +39,11 @@ for i in range(19):
     cl2.append(list2[i][1])
     cd2.append(list2[i][2])
     cm2.append(list2[i][3])
+    cl3.append(list3[i][1])
+    cd3.append(list3[i][2])
+    cm3.append(list3[i][3])
 
-
-# Interpolating cl, cd, cm
+# Interpolating cl, cd, cm to find a function describing each cl, cd, cm, at each AoA
 cly1 = sp.interpolate.interp1d(y, cl1, kind='cubic', fill_value="extrapolate")
 cdy1 = sp.interpolate.interp1d(y, cd1, kind='cubic', fill_value="extrapolate")
 cmy1 = sp.interpolate.interp1d(y, cm1, kind='cubic', fill_value="extrapolate")
@@ -57,47 +58,57 @@ cmy3 = sp.interpolate.interp1d(y, cm3, kind='cubic', fill_value="extrapolate")
 
 
 # -----------------Functions-------------------
-
-def c(y): # function describing the variation of the chord along the wingspan
+def c(y):
     '''
-
-    :param y: y distance along the wingspan starting from root
-    :return: chordlength
+    This function describes the variation of the chord along the wingspan.
+    :param y: Wing span [m]
+    :type y: float
+    :return: Chord length [m]
+    :rtype: float
     '''
     b = 31.1
     c = cr - (cr - ct) * 2 * y / b
     return c
 
 
-def Ldis(y, cly): # function describing the lift distribution
-    '''
-
-    :param y:
-    :param cly:
-    :return:
-    '''
+def Ldis(y, cly):
+    """
+    This function describes the lift distribution
+    :param y: Wing span [m]
+    :type y: float
+    :param cly: Interpolated function of the lift coefficient versus wing span
+    :type cly: array
+    :return: Lift distribution at a certain point along the wing span
+    :rtype: array
+    """
     Ldis = c(y) * q * cly(y)
     return Ldis
 
 
-def Ddis(y, cdy): # function describing the drag distribution
-    '''
-
-    :param y:
-    :param cdy:
-    :return:
-    '''
+def Ddis(y, cdy):
+    """
+    This function describes the drag distribution
+    :param y: Wing span [m]
+    :type y: float
+    :param cdy: Interpolated function of the drag coefficient versus wing span
+    :type cdy: array
+    :return: Drag distribution at a certain point along the wing span
+    :rtype: array
+    """
     Ddis = c(y) * q * cdy(y)
     return Ddis
 
 
-def Mdis(y, AoA):# function describing the moment distribution
-    '''
-
-    :param y:
-    :param cmy:
-    :return:
-    '''
+def Mdis(y, AoA):
+    """
+    This function describes the moment distribution
+    :param y: Wing span [m]
+    :type y: float
+    :param AoA: Angle of attack [deg]
+    :type AoA: float
+    :return: Moment distribution at a certain point along the wing span, based upon the angle of attack
+    :rtype: array
+    """
     Mdis = 0
     if AoA == 0:
         Mdis = (c(y)) **2 * q * cmy1(y)
@@ -128,11 +139,8 @@ def Ndis0(y, AoA):
     return Ndis
 
 
-# -------------------Distribution calculations------------------
-range = np.linspace(0, 15.55, 500)
-L1 = Ldis(range, cly1)
-D1 = Ddis(range, cdy1)
-M1 = Mdis(range, cmy1)
+# ------------------- Distribution calculations ------------------
+range = np.linspace(0, 15.55, 500)  # Wing span [m]
 
 L1 = Ldis(range, cly1)              # Lift distribution at AoA of 0 [deg]
 D1 = Ddis(range, cdy1)              # Drag distribution at AoA of 0 [deg]
@@ -143,7 +151,7 @@ D2 = Ddis(range, cdy2)              # Drag distribution at AoA of 10 [deg]
 M2 = Mdis(range, cmy2)              # Moment distribution at AoA of 10 [deg]
 
 # --------- Effect of velocity and angle of attack----------
-# From MainWing_a=0.00_v=10.00ms.txt, MainWing_a=10.00_v=10.00ms.txt
+# Following values result from XFLR5
 CL0 = 0.176815
 CL10 = 0.970715
 CD0 = 0.001319
@@ -157,41 +165,44 @@ CDd = 0.035
 CMd = 0.00000005
 
 
-def CLdy(y):  # y here would be the wingspan, "range"
+def CLdy(y):
     '''
-
-    :param y:
-    :return:
+    :param y: Wing span [m]
+    :type y: float
+    :return: Lift coefficient at a certain point along the wing span
+    :rtype: float
     '''
     CLdyvalue = cly1(y) + (CLd - CL0) / (CL10 - CL0) * (cly2(y) - cly1(y))
     return CLdyvalue
 
 
-def CDdy(y):  # y here would be the wingspan, "range"
+def CDdy(y):
     '''
-
-    :param y:
-    :return:
+    :param y: Wing span [m]
+    :type y: float
+    :return: Drag coefficient at a certain point along the wing span
+    :rtype: float
     '''
     CDdyvalue = cdy1(y) + (CDd - CD0) / (CD10 - CD0) * (cdy2(y) - cdy1(y))
     return CDdyvalue
 
 
-def CMdy(y):  # y here would be the wingspan, "range"
+def CMdy(y):
     '''
-
-    :param y:
-    :return:
+    :param y: Wing span [m]
+    :type y: float
+    :return: Moment coefficient at a certain point along the wing span
+    :rtype: float
     '''
     CMdyvalue = cmy1(y) + (CMd - CM0) / (CM10 - CM0) * (cmy2(y) - cmy1(y))
     return CMdyvalue
 
 
-def alphad():  # Design angle of attack
+def alphad():
     '''
-
-    :param y:
-    :return:
+    Function that gives the design angle of attack for a design lift coefficient.
+    :return: Design angle of attack
+    :rtype: float
     '''
     alphadvalue = np.degrees(np.arcsin((CLd - CL0) / (CL10 - CL0) * np.sin(np.radians(10))))
     return alphadvalue
@@ -199,11 +210,12 @@ def alphad():  # Design angle of attack
 '''
 # --------------------- Shear moment and bending diagrams -----------------------
 def Ny(AoA):
-    '''
-
-    :param AoA:
-    :return:
-    '''
+    
+    :param AoA: Angle of attack [deg]
+    :type AoA: float
+    :return: Normal force distribution
+    :rtype: array
+    
     if AoA == 0:
         L = L1
         D = D1
@@ -214,13 +226,12 @@ def Ny(AoA):
 
 
 def Ty(AoA):
-    """ # Example:
-    This function returns the sweep of the planform at any given percentage of the cord
-    :param c_percent: The percent of the cord (range from 0 to 1)
-    :type c_percent: float
-    :return: The sweep of the planform at a percentage of the cord [rad]
-    :rtype: float
-    """
+    
+    :param AoA: Angle of attack [deg]
+    :type AoA: float
+    :return: Tangential force distribution
+    :rtype: array
+    
     if AoA == 0:
         L = L1
         D = D1
@@ -228,99 +239,4 @@ def Ty(AoA):
         L = L2
         D = D2
     return np.sin(np.radians(AoA)) * L + np.cos(np.radians(AoA)) * D
-
-'''
-plt.plot(range, Ny(0))
-plt.title("Normal force distribution at 0 deg")
-# plt.show()
-
-plt.plot(range, Ty(0))
-plt.title("Tangential force distribution at 0 deg")
-# plt.show()
-
-plt.plot(range, Ny(10))
-plt.title("Normal force distribution at 10 deg")
-# plt.show()
-
-plt.plot(range, Ty(10))
-plt.title("Tangential force distribution at 10 deg")
-# plt.show()
-'''
-
-'''
-# ----------- Shear force diagram ------------
-def Ay(y):
-    t = 0.0005
-    return c(y) * 2.0580 * t
-
-
-def weight(y):
-    
-
-    :param y:
-    :return:
-    
-    [Vtot, Verr] = sp.integrate.quad(Ay, 0, 15.55)
-    Vfuel = 29174 / (800 * 2)  # Fuel density is 800 kg/m^3 as per WP 1
-    mwing = 1980 / 2
-
-    # rho = a * rho_fuel + b * rho_wing, a + b = 1
-    a = Vfuel / Vtot
-    b = 1 - a
-    rhofuel = 800  # kg/m^3
-    rhowing = 2700  # kg/m^3
-    rhotot = a * rhofuel + b * rhowing
-
-    Wy = rhotot * Ay(range) * 9.81
-    return Wy  # Return a single value from a function in the shape of "y=5*x+10"
-
-
-# -------------------- Total weight --------------------
-def wy0(y):
-    return Ny(0) - weight(y)
-
-
-def wy10(y):
-    return Ny(10) - weight(y)
-
-
-wy0inter = sp.interpolate.interp1d(list(range), list(wy0(range)), kind='cubic', fill_value="extrapolate")
-wy10inter = sp.interpolate.interp1d(list(range), list(wy10(range)), kind='cubic', fill_value="extrapolate")
-
-
-def wtotfunc(y):
-    return wy10inter(y)
-
-
-
-plt.plot(range, wy0inter(range))
-plt.title('Interpolated Wy0')
-plt.show()
-
-plt.plot(range, Ay(range))
-plt.title('Airfoil Area')
-plt.show()
-
-plt.plot(range, wy10inter(range))
-plt.title('Interpolated Wy10')
-plt.show()
-'''
-
-'''
-plt.plot(range, V0(range))
-plt.title('Shear force')
-plt.show()
-
-plt.plot(range, weight(range))
-plt.title("Weight diagrams")
-plt.show()
-'''
-
-'''
-Assumptions in this model:
-A(y) = P*t
-
-Problems:
-Shear force reaction force at root not included --> starts at 0 goes to max
-Solve by subtracting by total shear? gives negative shear, but it should be positive.
 '''
